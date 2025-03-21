a27c8632513296991846263b70e5d7d28a25f7de
FEELTHEVENOM <rahul.valli2003@gmail.com>
2025-03-21 21:49:45 +0530

    fixed report od drift

--- networksecurity/components/data_validation.py
+++ networksecurity/components/data_validation.py
@@ -70,9 +70,9 @@ class DataValidation:
                 d2 = currect_df[column]
                 is_same_dist = ks_2samp(d1,d2)
                 if threshold<=is_same_dist.pvalue:
-                    is_found = True
-                else:
                     is_found = False
+                else:
+                    is_found = True
                     status = False
                 report.update(
                     {
