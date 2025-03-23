577a2754e62a2b76ce65fb54b7168fd901265844
FEELTHEVENOM <rahul.valli2003@gmail.com>
2025-03-23 21:09:50 +0530

    Model Tracking is done using mlflow

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -22,6 +22,8 @@ from sklearn.ensemble import(
     RandomForestClassifier
 )
 
+import mlflow 
+
 logger = logging.getLogger('Model_Training')
 
 class ModelTrainer:
@@ -33,7 +35,18 @@ class ModelTrainer:
 
         except Exception as e:
             raise NetworkSecurityException(e, sys)
-        
+    
+    def track_mlflow(self, best_model, classificationmetric):
+        with mlflow.start_run():
+            f1_score = classificationmetric.f1_score
+            precission_score = classificationmetric.precision_score
+            recall_score = classificationmetric.recall_score
+
+            mlflow.log_metric("f1_score", f1_score)
+            mlflow.log_metric("precision", precission_score)
+            mlflow.log_metric("recall_score", recall_score)
+            mlflow.sklearn.log_model(best_model, "model")
+
     def train_model(self, x_train, y_train, x_test, y_test):
         try:
             models = {
@@ -81,9 +94,15 @@ class ModelTrainer:
 
             classificaton_train_metric = get_classification_score(y_true=y_train, y_pred= y_train_pred)
 
+            # MLFLOW INTERGRATION FOR TRACKING IN TRAINING 
+            self.track_mlflow(best_model, classificaton_train_metric)
+
             y_test_pred = best_model.predict(x_test)
 
             classificaton_test_metric = get_classification_score(y_true=y_test, y_pred= y_test_pred)
+
+            # MLFLOW INTERGRATION FOR TRACKING IN TRAINING 
+            self.track_mlflow(best_model, classificaton_test_metric)
 
             preprocessor = load_object(file_path=self.data_transformation_artifact.transformed_object_file_path)
 
