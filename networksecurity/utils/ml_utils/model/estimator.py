4015afdc392b2f8dd75a7b2373acecca65a85d9f
FEELTHEVENOM <rahul.valli2003@gmail.com>
2025-03-24 11:38:33 +0530

    Added prediction pipeline using FastAPI

--- networksecurity/utils/ml_utils/model/estimator.py
+++ networksecurity/utils/ml_utils/model/estimator.py
@@ -16,7 +16,7 @@ class NetworkModel:
         
     def predict(self, x):
         try:
-            x_transform = self.preprocess(x)
+            x_transform = self.preprocess.transform(x)
             y_hat = self.model.predict(x_transform)
             return y_hat
         except Exception as e:
